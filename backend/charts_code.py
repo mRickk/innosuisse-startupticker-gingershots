de7e4f8388774bae718aa9b73e91904e2b40ba9b
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2025-04-12 19:50:33 +0200

    Minor changes

--- backend/charts_code.py
+++ backend/charts_code.py
@@ -4,13 +4,15 @@ from huggingface_hub import login
 from openai import OpenAI
 import json
 
+token = ""
 with open("hf/hf_access_token.txt", 'r') as file:
     token = file.read().strip()
     login(token=token)
 
 # THIS FUNCTION WILL BE CHANGED IN THE API BRANCH
 def generate_charts_code(question, query, df):
-    input_text = """You are a data analyst.
+    df = df.to_string()
+    input_text = f"""You are a data analyst.
     You are given the original user question, the corresponding database query and a Pandas dataframe of the retrieved data.
     Starting from the dataframe data, write JavaScript code using Charts.js to generate interactive charts (multiple if needed) that are relevant to the data.
     Do not put dependencies, just write the code that can be run directly inside another web application.
@@ -26,13 +28,15 @@ def generate_charts_code(question, query, df):
     {df}
     
 
-    Output following this format:
-    [{"code": "<insert code 1>", "description": "<insert description 1>"}, {"code": "<insert code 2>", "description": "<insert description 2>"}, ...]
+    IMPORTANT: Only output following this format
+    [{{"code": "<insert code 1>", "description": "<insert description 1>"}},
+    {{"code": "<insert code 2>", "description": "<insert description 2>"}},
+    ...]
     """
 
     client = OpenAI(
             base_url = "https://mjc2xhrx4nq9sqbo.us-east-1.aws.endpoints.huggingface.cloud/v1/",
-            api_key = "" # Replace with your actual API key
+            api_key = token # Replace with your actual API key
         )
 
     chat_completion = client.chat.completions.create(
@@ -40,12 +44,12 @@ def generate_charts_code(question, query, df):
         messages=[
         {
             "role": "user",
-            "content": "Tell me about Zurich"
+            "content": input_text
         }
     ],
         top_p=None,
         temperature=None,
-        max_tokens=150,
+        max_tokens=2000,
         stream=True,
         seed=None,
         stop=None,
@@ -56,6 +60,8 @@ def generate_charts_code(question, query, df):
     response = ""
     for message in chat_completion:
         response += message.choices[0].delta.content
+
+    response = response.spli("</think>")[1]
 
     try:
         # Try to parse the response as JSON
